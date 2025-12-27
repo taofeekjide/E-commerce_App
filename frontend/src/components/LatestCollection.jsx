e2e2bd84949fb5ae159fb50c55c2ee23d55e937b
Tao02 <idowutaofeek16@gmail.com>
2025-12-27 21:52:02 +0100

    smalls

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -14,7 +14,7 @@ export default function LatestCollection() {
   return (
     <div className="my-10">
       <div className="text-center py-8 text-3xl">
-        <Title text1={"Latest"} text2={"Collection"} />
+        <Title text1={"Latest"} text2={"Collections"} />
         <p className="w-3/4 m-auto text-xs sm:text sm:text-sm md:text-base text-gray-600">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo
           natus, porro excepturi recusandae adipisci dignissimos odio voluptas,
