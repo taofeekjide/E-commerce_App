aa02793407f1bf055fc59212b041c03cd20487ca
Tao02 <idowutaofeek16@gmail.com>
2025-12-27 21:51:40 +0100

    Create latest collections component

---
+++ frontend/src/components/ProductItem.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+import { ShopContext } from "../context/shopContext";
+import { Link } from "react-router-dom";
+
+export default function ProductItem({ id, name, image, price }) {
+  const { currency } = useContext(ShopContext);
+
+  return (
+    <Link className="text-gray-700 cursor-pointer" to={`/product/${id}`}>
+      <div className="overflow-hidden">
+        <img
+          className="hover:scale-110 transition ease-in-out"
+          src={image[0]}
+          alt=""
+        />
+      </div>
+      <p className="pt-3 pb-1 text-sm">{name}</p>
+      <p className="text-sm font-medium">
+        {currency}
+        {price}
+      </p>
+    </Link>
+  );
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import LatestCollection from '../components/latestCollection'
 
 export default function Home() {
   return (
     <div>
       <Hero/>
+      <LatestCollection/>
     </div>
   )
 }
