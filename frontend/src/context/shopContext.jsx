460497cc88c97188052ba0627bc3fc877d07b587
Tao02 <idowutaofeek16@gmail.com>
2025-12-31 21:46:47 +0100

    Create the product selected page, and the related products component

--- frontend/src/context/shopContext.jsx
+++ frontend/src/context/shopContext.jsx
@@ -5,7 +5,10 @@ export const ShopContext = createContext();
 const products = [
   {
     _id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    image: [
+      "/images/products/athletic-cotton-socks-6-pairs.jpg",
+      "/images/products/intermediate-composite-basketball.jpg",
+    ],
     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
     rating: {
       stars: 4.5,
@@ -13,10 +16,13 @@ const products = [
     },
     price: 1090,
     keywords: ["socks", "sports", "apparel"],
+    sizes: ["large", "small"],
+    category: "Men",
+    subCategory: "Topwear",
   },
   {
     _id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    image: "images/products/intermediate-composite-basketball.jpg",
+    image: ["/images/products/intermediate-composite-basketball.jpg"],
     name: "Intermediate Size Basketball",
     rating: {
       stars: 4,
@@ -24,10 +30,13 @@ const products = [
     },
     price: 2095,
     keywords: ["sports", "basketballs"],
+    sizes: ["large", "small"],
+    category: "Men",
+    subCategory: "Topwear",
   },
   {
     _id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    image: ["/images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg"],
     name: "Adults Plain Cotton T-Shirt - 2 Pack",
     rating: {
       stars: 4.5,
@@ -37,10 +46,13 @@ const products = [
     keywords: ["tshirts", "apparel", "mens"],
     type: "clothing",
     sizeChartLink: "images/clothing-size-chart.png",
+    sizes: ["large", "small"],
+    category: "Men",
+    subCategory: "Topwear",
   },
   {
     _id: "54e0eccd-8f36-462b-b68a-8182611d9add",
-    image: "images/products/black-2-slot-toaster.jpg",
+    image: ["/images/products/black-2-slot-toaster.jpg"],
     name: "2 Slot Toaster - Black",
     rating: {
       stars: 5,
@@ -48,10 +60,13 @@ const products = [
     },
     price: 1899,
     keywords: ["toaster", "kitchen", "appliances"],
+    sizes: ["large", "small"],
+    category: "Men",
+    subCategory: "Topwear",
   },
   {
     _id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
-    image: "images/products/6-piece-white-dinner-plate-set.jpg",
+    image: ["/images/products/6-piece-white-dinner-plate-set.jpg"],
     name: "6 Piece White Dinner Plate Set",
     rating: {
       stars: 4,
@@ -59,6 +74,9 @@ const products = [
     },
     price: 2067,
     keywords: ["plates", "kitchen", "dining"],
+    sizes: ["large", "small"],
+    category: "Men",
+    subCategory: "Topwear",
   },
   {
     _id: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,7 +1,123 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
+import { Star, StarHalf } from "lucide-react";
+import RelatedProducts from "../components/RelatedProducts";
 
 export default function Product() {
-  return (
-    <div>Product</div>
-  )
+  const { productId } = useParams();
+  const { products, currency } = useContext(ShopContext);
+  const [productData, setProductData] = useState(false);
+  const [image, setImage] = useState("");
+  const [size, setSize] = useState("");
+
+  async function fetchProductData() {
+    products.map((item) => {
+      if (item._id === productId) {
+        setProductData(item);
+        setImage(item.image[0]);
+        return null;
+      }
+    });
+  }
+
+  useEffect(() => {
+    fetchProductData();
+  }, [productId, products]);
+
+  return productData ? (
+    <div className="border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100">
+      {/* Product Data */}
+      <div className="flex gap-12 sm:gap-12 flex-col sm:flex-row">
+        {/* Product images */}
+        <div className="flex-1 flex flex-col-reverse gap-3 sm:flex-row">
+          <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full">
+            {productData.image.map((item, index) => (
+              <img
+                onClick={() => setImage(item)}
+                src={item}
+                key={index}
+                className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer "
+                alt=""
+              />
+            ))}
+          </div>
+          <div className="w-full sm:w-[80%]">
+            <img src={image} alt="" className="w-full h-auto object-cover" />
+          </div>
+        </div>
+        {/* Product Details */}
+        <div className="flex-1">
+          <h1 className="font-medium text-2xl mt-2">{productData.name}</h1>
+          <div className="flex items-center gap-1 mt-2">
+            <Star className="w-3.5" />
+            <Star className="w-3.5" />
+            <Star className="w-3.5" />
+            <Star className="w-3.5" />
+            <StarHalf className="w-3.5" />
+            <p className="pl-2">(122)</p>
+          </div>
+          <p className="mt-5 text-3xl font-medium">
+            {currency}
+            {productData.price}{" "}
+          </p>
+          <p className="mt-5 text-gray-500 md:w-4/5">
+            {" "}
+            {productData.description}{" "}
+          </p>
+          <div className="flex flex-col gap-4 my-8">
+            <p>Select Size</p>
+            <div className="flex gap-2">
+              {productData.sizes.map((item, index) => (
+                <button
+                  onClick={() => setSize(item)}
+                  className={`border py-2 px-4 bg-gray-100 ${
+                    item === size ? "border-orange-500" : ""
+                  }`}
+                  key={index}
+                >
+                  {item}
+                </button>
+              ))}
+            </div>
+          </div>
+          <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">
+            {" "}
+            Add To Cart
+          </button>
+          <hr className="mt-8 sm:w-4/5" />
+          <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
+            <p>100% Original Product</p>
+            <p>Cash On Delivery is not available</p>
+            <p>Easy return and exchange policy within 24hrs</p>
+          </div>
+        </div>
+      </div>
+      {/* Description and Review */}
+      <div className="mt-20">
+        <div className="flex">
+          <b className="border px-5 py-3 text-sm">Description</b>
+          <p className="border px-5 py-3 text-sm"> Reviews (122)</p>
+        </div>
+        <div className="flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae
+            similique hic maiores minus ipsum neque, mollitia ratione in aut
+            pariatur autem, voluptas magnam voluptatibus amet est veniam
+            repellendus tenetur rerum?
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam
+            perspiciatis eum debitis temporibus nobis? Reprehenderit magni
+            ducimus doloribus, porro sapiente nesciunt quidem, ea nobis id nam
+            vitae dolores explicabo ad.
+          </p>
+        </div>
+      </div>
+      {/* Display Related Products */}
+      <RelatedProducts category={productData.category} subCategory={productData.subCategory} />
+    </div>
+  ) : (
+    <div className="opacity-0"></div>
+  );
 }
